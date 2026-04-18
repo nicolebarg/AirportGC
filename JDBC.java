ff7480116f40dc6988ac38909dbed1a8be438cfb
nicolebarg <161248771+nicolebarg@users.noreply.github.com>
2026-04-18 10:10:52 +0300

    Add files via upload

---
+++ JDBC.java
@@ -0,0 +1,160 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class JDBC {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+
+        //User data
+        String dbUrl = "jdbc:postgresql://localhost:5432/AirportGC";
+        String dbUsername = "postgres";
+        String dbPassword = "1234";
+
+        //Queries
+        
+
+        //Set Connection
+        connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        
+        //Statement
+        Statement statement = connection.createStatement();
+
+        //Execute query and get ResultSet (The table)
+        
+        
+
+        //create main menu & create functions for queries that accept user input (Insert func, Select func, etc)
+
+        //Main menu
+        Boolean menuRunning = true;
+
+        while (menuRunning) {
+            System.out.println("\nPick an option.\n" +
+                                "1. View Current table.\n" +
+                                "2. Add new Data to table.\n" +
+                                "3. remove Data from table.\n" + 
+                                "4. Change data in table.\n" +
+                                "5. Select a different option.\n" +
+                                "6. Delete all data from table.\n" +
+                                "7. Exit programm.\n");
+            Scanner scn = new Scanner(System.in);
+            int option = scn.nextInt();
+                
+            switch (option) {
+                case 1: //SELECT *
+
+                    String selectQuery = "SELECT * FROM gc";
+                    ResultSet resultSet1 = statement.executeQuery(selectQuery);
+                
+                    while(resultSet1.next()){
+                    System.out.println("\n"+ resultSet1.getString("flight_id") + " - " + resultSet1.getString("plane_id") + 
+                                        " - " + resultSet1.getString("plane_status") + 
+                                        " - " + resultSet1.getString("gate") + 
+                                        " - " + resultSet1.getString("route") + 
+                                        " - " + resultSet1.getTime("time") + 
+                                        " - " + resultSet1.getDate("date"));
+                    }
+                    
+                    continue;
+                case 2: //INSERT new flight
+
+                    System.out.println("\nEnter flight id: ");
+                    scn.nextLine();
+                    String flight_id = scn.nextLine();
+                    System.out.println("Enter plane_id: ");
+                    String plane_id = scn.nextLine();
+                    System.out.println("Enter plane_status (Arriving/Boarding/Departuring): ");
+                    String plane_status = scn.nextLine();
+                    System.out.println("Enter gate(arr/dep): ");
+                    String gate = scn.nextLine();
+                    System.out.println("Enter route(arr/dep): ");
+                    String route = scn.nextLine();
+                    System.out.println("Enter time(arr/dep) in format HH:MM : ");
+                    String time_str = scn.nextLine();
+                    LocalTime time = LocalTime.parse(time_str);
+                    System.out.println("Enter date(arr/dep) in format yyyy-mm-dd: ");
+                    String date_str = scn.nextLine();
+                    LocalDate date = LocalDate.parse(date_str);
+                    String insertQuery = "INSERT INTO gc(flight_id,plane_id,plane_status,gate,route,time,date) VALUES(?,?,?,?,?,?,?)";
+
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                        preparedStatement.setString(1,flight_id);
+                        preparedStatement.setString(2,plane_id);
+                        preparedStatement.setString(3,plane_status);
+                        preparedStatement.setString(4,gate);
+                        preparedStatement.setString(5,route);
+                        preparedStatement.setObject(6,time);
+                        preparedStatement.setObject(7,date);
+
+                        preparedStatement.executeUpdate();
+                        System.out.println("\nRow added successfully");
+                    } catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                    
+                    continue;
+
+                case 3://REMOVE single id
+
+                    System.out.println("\nEnter flight ID to remove from table: ");
+                    scn.nextLine();
+                    String idToRemove = scn.nextLine();
+                    String removeRowQuery = "DELETE FROM gc WHERE flight_id = "+"'" +idToRemove+"'";
+                    statement.executeUpdate(removeRowQuery);
+                    System.out.println("\nRow with flight_id: "+idToRemove+" was removed.");
+
+                    continue;
+                
+                    case 4://ALTER data
+
+                        System.out.println("Enter flight ID to change value: ");
+                        scn.nextLine();
+                        String idToAlter = scn.nextLine();
+                        System.out.println("Enter column name to change it's value: ");
+                        String colToAlter = scn.nextLine();
+                        System.out.println("Enter new data to replace with: ");
+                        String replacementData = scn.nextLine();
+                        
+                        String alterDataQuery;
+
+                        if (colToAlter == "date") {
+                            LocalDate replacementDate = LocalDate.parse(replacementData);
+                            alterDataQuery = "UPDATE gc SET " +colToAlter+ " = "+"'" +replacementDate+"'"+ " WHERE flight_id = "+"'" +idToAlter+"'";
+                        }
+                        else if (colToAlter == "time"){
+                            LocalTime replacemenTime = LocalTime.parse(replacementData);
+                            alterDataQuery = "UPDATE gc SET " +colToAlter+ " = "+"'" +replacemenTime+"'"+ " WHERE flight_id = "+"'" +idToAlter+"'";
+                        }
+                        else{
+                            alterDataQuery = "UPDATE gc SET " +colToAlter+ " = "+"'" +replacementData+"'"+ " WHERE flight_id = "+"'" +idToAlter+"'";
+                        }
+                        statement.executeUpdate(alterDataQuery);
+                        System.out.println("\nThe change was successful.");
+
+                        continue;
+
+                case 5:
+                    continue;
+
+                case 6:
+                    String deleteAllDataQuery = "DELETE FROM gc";
+                    statement.executeUpdate(deleteAllDataQuery);
+
+                    continue;
+
+                case 7:
+                    System.out.println("\ngood Bye!");
+                    menuRunning = false;
+  
+            }
+            scn.close();
+        }
+        //Close connection
+        
+        connection.close();
+
+    }
+}
